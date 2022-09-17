81c16a46c21253041d374154ca256a9eded7595e
Chuck Grindel <chuck.grindel@gmail.com>
2022-09-17 15:06:10 -0600

    Add `generate` command (#5)
    The generate command prints "Hello, World!"

--- Sources/SPMBazel/SPMBazel.swift
+++ Sources/SPMBazel/SPMBazel.swift
@@ -1,25 +1,10 @@
 import ArgumentParser
 import Foundation
 
-struct SPMBazel: AsyncParsableCommand {
-  mutating func runAsync() async throws {
-    guard let output = "Hello, World!".data(using: .utf8) else {
-      return
-    }
-    try writeOutput { fileHandle in
-      try fileHandle.write(contentsOf: output)
-    }
-  }
-
-  private func createFileHandle() throws -> FileHandle {
-    return .standardOutput
-  }
-
-  func writeOutput(closure: (_ fileHandle: FileHandle) throws -> Void) throws {
-    let fileHandle = try createFileHandle()
-    defer {
-      try? fileHandle.close()
-    }
-    try closure(fileHandle)
-  }
+struct SPMBazel: ParsableCommand {
+  static var configuration = CommandConfiguration(
+    abstract: "Generate and manage Swift dependencies for Bazel.",
+    subcommands: [Generate.self],
+    defaultSubcommand: Generate.self
+  )
 }
