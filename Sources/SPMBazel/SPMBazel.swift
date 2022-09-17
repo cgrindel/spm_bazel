ac63b20f2affdbb71c43ec5d89a19b0bd58e36a7
Chuck Grindel <chuck@chuckgrindel.com>
2022-09-17 15:00:28 -0600

    Clean up

--- Sources/SPMBazel/SPMBazel.swift
+++ Sources/SPMBazel/SPMBazel.swift
@@ -1,7 +1,6 @@
 import ArgumentParser
 import Foundation
 
-// struct SPMBazel: AsyncParsableCommand {
 struct SPMBazel: ParsableCommand {
   static var configuration = CommandConfiguration(
     abstract: "Generate and manage Swift dependencies for Bazel.",
